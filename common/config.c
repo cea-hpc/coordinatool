5e754d90d9a5a00fd80e5a2424f070efc5c07e0b
Dominique Martinet <asmadeus@codewreck.org>
2021-11-06 09:10:18 +0000

    move common config parsing helpers to common/config
    tbe actual parsing itself cannot be shared (both sides won't accept all options) but most
    of it can be reused
    
    Change-Id: I146567be860155534b2d81b18871a4e1cc0c5a34
    Reviewed-on: https://review.gerrithub.io/c/cea-hpc/coordinatool/+/526901
    Tested-by: Dominique Martinet <asmadeus@codewreck.org>
    Reviewed-by: VALERI Yoann <yoann.valeri@cea.fr>
    Reviewed-by: Dominique Martinet <asmadeus@codewreck.org>
    Reviewed-by: Guillaume Courrier <guillaume.courrier@cea.fr>

---
+++ common/config.c
@@ -0,0 +1,116 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+
+#include <stdlib.h>
+#include <strings.h>
+
+#include "logs.h"
+
+int getenv_str(const char *name, const char **val) {
+	const char *env = getenv(name);
+	if (!env)
+		return 0;
+	*val = env;
+	LOG_INFO("env setting %s to %s\n", name, env);
+	return 1;
+}
+
+long long str_suffix_to_u32(const char *str, const char *error_hint) {
+	char *endptr;
+
+	long long val = strtoll(str, &endptr, 0);
+	long multiplier = 1;
+
+	if (!endptr)
+		abort();
+	switch (*endptr) {
+	case 0:
+		break;
+	case 'g':
+	case 'G':
+		multiplier *= 1024;
+		// fallthrough
+	case 'm':
+	case 'M':
+		multiplier *= 1024;
+		// fallthrough
+	case 'k':
+	case 'K':
+		multiplier *= 1024;
+		if (endptr[1] != 0) {
+			LOG_WARN("trailing data after size prefix: %s, continuing anyway",
+				  endptr + 1);
+		}
+		break;
+	default:
+		LOG_ERROR(-EINVAL, "%s was set to %s, which has trailing %s",
+			  error_hint, str, endptr);
+		return -EINVAL;
+	}
+
+	/* allow -1 as max */
+	if (val == -1)
+		return UINT32_MAX;
+
+	if (val > UINT32_MAX / multiplier || val < 0) {
+		LOG_ERROR(-EINVAL, "%s was set to %s, which would overflow",
+			  error_hint, str);
+		return -EINVAL;
+	}
+	return val * multiplier;
+}
+
+int getenv_u32(const char *name, uint32_t *val) {
+	const char *env = getenv(name);
+	if (!env)
+		return 0;
+
+	long long envval = str_suffix_to_u32(env, name);
+	if (envval < 0)
+		return envval;
+
+	*val = envval;
+	LOG_INFO("env setting %s to %u\n", name, *val);
+	return 1;
+}
+
+enum llapi_message_level str_to_verbose(const char *str) {
+	if (!strcasecmp(str, "off")) {
+		return LLAPI_MSG_OFF;
+	}
+	if (!strcasecmp(str, "fatal")) {
+		return LLAPI_MSG_FATAL;
+	}
+	if (!strcasecmp(str, "error")) {
+		return LLAPI_MSG_ERROR;
+	}
+	if (!strcasecmp(str, "warn")) {
+		return LLAPI_MSG_WARN;
+	}
+	if (!strcasecmp(str, "normal")) {
+		return LLAPI_MSG_NORMAL;
+	}
+	if (!strcasecmp(str, "info")) {
+		return LLAPI_MSG_INFO;
+	}
+	if (!strcasecmp(str, "debug")) {
+		return LLAPI_MSG_DEBUG;
+	}
+	LOG_ERROR(-EINVAL, "invalid debug level: %s", str);
+	return -1;
+}
+
+int getenv_verbose(const char *name, int *val) {
+	const char *env = getenv(name);
+	if (!env)
+		return 0;
+
+	int verbose = str_to_verbose(env);
+	if (verbose < 0)
+		return verbose;
+
+	*val = verbose;
+	llapi_msg_set_level(verbose);
+	return 1;
+}
+
+
