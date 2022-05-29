e3700664d374ea251ab19fd0d99a02d450329b7e
Dominique Martinet <asmadeus@codewreck.org>
2022-05-29 01:58:04 +0000

    coordinatool: add redis boilerplate
    Change-Id: I07a361664f9c00adbe1107ce14bff7d7ada87e88

---
+++ copytool/redis.c
@@ -0,0 +1,66 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+
+#include <sys/epoll.h>
+
+#include "coordinatool.h"
+
+static void redis_addwrite(void *_state) {
+	struct state *state = _state;
+	struct epoll_event ev;
+
+	ev.events = EPOLLIN|EPOLLOUT;
+	ev.data.ptr = state->redis_ac;
+
+	int rc = epoll_ctl(state->epoll_fd, EPOLL_CTL_MOD,
+			   state->redis_ac->c.fd, &ev);
+	if (rc < 0)
+		LOG_WARN(-errno, "Could not listen redis fd for write: redis broken!");
+}
+
+static void redis_delwrite(void *_state) {
+	struct state *state = _state;
+	struct epoll_event ev;
+
+	ev.events = EPOLLIN;
+	ev.data.ptr = state->redis_ac;
+
+	int rc = epoll_ctl(state->epoll_fd, EPOLL_CTL_MOD,
+			   state->redis_ac->c.fd, &ev);
+	if (rc < 0)
+		LOG_WARN(-errno, "Could not stop listening redis fd for write?!");
+}
+
+int redis_connect(struct state *state) {
+	int rc;
+	redisAsyncContext *ac;
+
+	// XXX options for redis ip/port
+	ac = redisAsyncConnect("127.0.0.1", 6379);
+	if (!ac)
+	       abort(); // ENOMEM
+	if (ac->err) {
+		// XXX ac->errstr 
+		return -1;
+	}
+	state->redis_ac = ac;
+
+	/* We have our own event loop, so we need to register our own callbacks...
+	 * Switch to libev(ent) at some point?
+	 * For now it's just as easy to use directly... */
+	ac->ev.data = state;
+	ac->ev.addWrite = redis_addwrite;
+	ac->ev.delWrite = redis_delwrite;
+	// delRead never used
+	// addRead could be used for epoll oneshot, but we're not oneshot
+	// cleanup only used on disconnect, we can do it ourselves
+	// scheduleTimeout could be useful though...
+	//   if that is useful switching to libev sounds better than adding
+	//   a timerfd
+
+	rc = epoll_addfd(state->epoll_fd, state->redis_ac->c.fd,
+			 state->redis_ac);
+	if (rc < 0)
+		return rc;
+
+	return 0;
+}
