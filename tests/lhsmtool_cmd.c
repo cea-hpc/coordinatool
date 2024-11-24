98c645c9b79433e2283ce415c7c9c67c65363ba8
Dominique Martinet <asmadeus@codewreck.org>
2024-11-24 20:35:41 +0900

    lhsmtool_cmd: fix g_shell_parse_argv error if err != NULL
    g_shell_parse_argv checks that if a valid err pointer is given then it is NULL
    (so as to not overwrite another error), depending on the compiler and the phase
    moon this apparently has always been working up till now but this lead to
    segfault trying to free the uninitialized value in a test, so always
    properly init the value to NULL.
    
    Change-Id: Ic996453cf202b93613a6aaff8a55f9cde25811c5

--- tests/lhsmtool_cmd.c
+++ tests/lhsmtool_cmd.c
@@ -532,7 +532,7 @@ static int ct_hsm_io_cmd(const enum hsm_copytool_action hsma, GMainLoop *loop,
 			 const long hal_flags)
 {
 	struct cmd_cb_args *cb_args;
-	GError *err;
+	GError *err = NULL;
 	GPid pid;
 	GSource *timer_gsrc;
 	GSource *term_gsrc;
