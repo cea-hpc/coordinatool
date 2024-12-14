f8e8659ea2666558ea665364d12a054bf7ada909
Dominique Martinet <asmadeus@codewreck.org>
2024-12-14 13:37:23 +0900

    copytool: schedule_client: refactor list chaining
    schedule_client had an open-coded 'cds_lists_for_each_safe' for two
    lists in a row, going through the next list after the current one was
    done.
    With batches coming, we want archives to not go through the later lists
    if there was no batch available, and the number of lists will be
    variable, so make this processing more generic with an array of lists.
    
    (We also split it to a new header as it will be used in another file
    later)
    
    This commit in itself should not change behaviour.
    
    Change-Id: I2c8fd0f2ac225d7e66ea6be9326286658d40212e

---
+++ common/list_utils.h
@@ -0,0 +1,29 @@
+/* SPDX-License-Identifier: LGPL-3.0-or-later */
+
+#pragma once
+
+/* compiler.h is included in list.h for newer versions of urcu, but
+ * we cannot depend on it yet so just re-include it for caa_container_of */
+#include <urcu/compiler.h>
+#include <urcu/list.h>
+
+/* get next item from a list of list. Note the list is updated! */
+static inline struct cds_list_head *
+cds_manylists_next(struct cds_list_head *node, struct cds_list_head ***heads_p)
+{
+	struct cds_list_head **heads = *heads_p;
+	while (node && node->next == heads[0]) {
+		heads++;
+		node = heads[0];
+	}
+	*heads_p = heads;
+	return node ? node->next : NULL;
+}
+
+/* It's a bit of a pain to chain multiple lists in cds_for_each(_safe) manually,
+ * so this helper takes a list of lists and walks through them all. */
+#define cds_manylists_for_each_safe(pos, p, heads_init)  \
+	struct cds_list_head **heads = (heads_init);     \
+	for (pos = cds_manylists_next(heads[0], &heads), \
+	    p = cds_manylists_next(pos, &heads);         \
+	     pos; pos = p, p = cds_manylists_next(p, &heads))
