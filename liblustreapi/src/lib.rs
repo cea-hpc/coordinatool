a09b36d9e1030ef0e726b1fa9c1c6f7981f7e5a9
Dominique Martinet <asmadeus@codewreck.org>
2025-02-14 21:53:15 +0900

    rust: fix build on el9
    el9 uses older rust, fix and use it for dev

--- liblustreapi/src/lib.rs
+++ liblustreapi/src/lib.rs
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 
+// XXX only needed for older rust (128 bit FFI)
+#![allow(improper_ctypes)]
 // non-standard names generated
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
