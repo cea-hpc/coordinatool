fc9d6729b8b0ad5070ed1e3eb48914c0b1421276
Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>
2026-02-11 16:27:22 +0100

    hash consistency: replace the tag value with hash(tag_value) % N
    A new parameter has been added to archive_on_hosts_ch to specify
    whether the tag value should be replaced by its hash.
    
     - If the parameter is 0, the previous behavior is preserved.
    
     - Otherwise, the tag value is replaced by hash(tag_value) % N, where N
       is the value specified in the archive_on_hosts_ch parameter.
    
    The hash is computed before applying hash consistency, to avoid
    sending the same hash to two different data movers.
    
    Change-Id: I95b520a57224516f07ca9fd01c763bd7f6ccb6fa
    Signed-off-by: Gauthier EVRAERD <Gauthier.EVRAERD@cea.fr>

--- copytool/utils.c
+++ copytool/utils.c
@@ -38,3 +38,38 @@ char *parse_hint(struct hsm_action_node *han, const char *hint_needle,
 
 	return hint;
 }
+
+size_t dbj2(const char *buf, size_t size)
+{
+	size_t hash = 5381;
+
+	for (size_t i = 0; i < size; i++)
+		hash = ((hash << 5) + hash) + buf[i];
+
+	return hash;
+}
+
+char *replace_string(const char *orig, size_t orig_len, const char *new_value,
+		     size_t new_len, const char *old_value, size_t old_len)
+{
+	char *string, *ptr;
+	size_t len;
+
+	len = new_len + (old_value - orig) +
+		((orig + orig_len) - (old_value + old_len));
+
+	string = xmalloc(len + 1);
+	string[len] = '\0';
+
+	ptr = string;
+	memcpy(ptr, orig, old_value - orig);
+	ptr += (old_value - orig);
+
+	memcpy(ptr, new_value, new_len);
+	ptr += new_len;
+
+	memcpy(ptr, old_value + old_len,
+	       ((orig + orig_len) - (old_value + old_len)));
+
+	return string;
+}
